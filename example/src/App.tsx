1913bd18a99b2140d0c1ad52a2ff1244e92463cb
Sanjeev Yadav <sanjeevy133@protonmail.com>
2022-02-11 08:39:14 +0530

    Write Example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,77 @@
-import React, { useEffect } from 'react'
-import { Counter } from 'react-native-date-time-picker'
+import React, { useState } from 'react';
+import { View, Button, Modal, StyleSheet } from 'react-native';
+import DateTimePicker from 'react-native-date-time-picker';
 
 const App = () => {
-  useEffect(() => {
-    console.log('')
-  })
+    const [date, setDate] = useState(new Date(1950, 6, 15));
+    const [mode, setMode] = useState<'date' | 'time'>('date');
+    const [show, setShow] = useState(false);
 
-  return <Counter />
-}
+    const onConfirm = (selectedDate: Date) => {
+        setShow(false);
+        setDate(selectedDate);
+    };
 
-export default App
+    const showMode = (currentMode: 'date' | 'time') => {
+        setShow(true);
+        setMode(currentMode);
+    };
+
+    const showDatepicker = () => {
+        showMode('date');
+    };
+
+    const showTimepicker = () => {
+        showMode('time');
+    };
+
+    const closeModal = () => {
+        setShow(false);
+    };
+
+    return (
+        <View style={styles.container}>
+            <Button onPress={showDatepicker} title="Show date picker!" />
+            <View style={styles.divider} />
+            <Button onPress={showTimepicker} title="Show time picker!" />
+            <Modal transparent animationType="slide" visible={show} onRequestClose={closeModal}>
+                <View style={styles.centeredView}>
+                    <View style={styles.card}>
+                        <DateTimePicker
+                            value={date}
+                            mode={mode}
+                            is24Hour={true}
+                            onConfirm={onConfirm}
+                            onClose={closeModal}
+                        />
+                    </View>
+                </View>
+            </Modal>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 32,
+        flex: 1,
+        backgroundColor: 'black',
+        justifyContent: 'center',
+    },
+    divider: {
+        marginBottom: 32,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0,0,0,.7)',
+    },
+    card: {
+        backgroundColor: 'white',
+        padding: 24,
+        margin: 16,
+        borderRadius: 4,
+    },
+});
+
+export default App;
